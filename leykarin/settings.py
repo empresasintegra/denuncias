f14fb5f1d36c732c62eed4b597e33f696330eb95
EstebanRisopatronIT <esteban.risopatron@empresasintegra.cl>
2025-08-22 09:30:41 -0400

    quitando ssl disable

--- leykarin/settings.py
+++ leykarin/settings.py
@@ -131,7 +131,6 @@ DATABASES = {
         'PASSWORD':  os.getenv('DB_PASSWORD'),
         'HOST':   os.getenv('DB_HOST'),
         'PORT':  os.getenv('DB_PORT'),
-        "OPTIONS": {"sslmode": "disable"}, 
     }
 }
 
